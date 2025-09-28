1b7d35676d7b34dc9a87c457502748a989a695f8
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2025-09-28 14:26:58 +0200

    chore: remove unused example code

--- test-rust-project/examples/hello_world.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("Hello from example!");
-    println!("This is an example target that can be run with 'cargo run --example hello_world'");
-}
--- test-rust-project/src/main.rs
+++
@@ -1,6 +0,0 @@
-use testlib::greet;
-
-fn main() {
-    println!("Hello from main!");
-    greet("World");
-}
