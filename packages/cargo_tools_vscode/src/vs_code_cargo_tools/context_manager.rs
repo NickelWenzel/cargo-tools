6853567edb74d051ea6e38cc521f441f641ccd42
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2025-11-09 22:14:20 +0100

    feat: implement context manager

--- packages/cargo_tools_vscode/src/vs_code_cargo_tools/context_manager.rs
+++ packages/cargo_tools_vscode/src/vs_code_cargo_tools/context_manager.rs
@@ -1,3 +1,4 @@
+use crate::vs_code_api;
 use crate::vs_code_state_manager::VSCodeStateManager;
 
 /// VS Code-specific implementation of ContextManager.
@@ -17,12 +18,12 @@ impl VSCodeContextManager {
 impl cargo_tools::cargo_tools::ContextManager for VSCodeContextManager {
     type StateManagerT = VSCodeStateManager;
 
-    fn set_is_cargo_project(&self, _is_cargo_project: bool) {
-        todo!("Set cargo project context in VS Code")
+    fn set_is_cargo_project(&self, is_cargo_project: bool) {
+        vs_code_api::set_cargo_context(is_cargo_project);
     }
 
-    fn set_has_makefile(&self, _has_makefile: bool) {
-        todo!("Set makefile context in VS Code")
+    fn set_has_makefile(&self, has_makefile: bool) {
+        vs_code_api::set_makefile_context(has_makefile);
     }
 
     fn state_manager(&self) -> &Self::StateManagerT {
