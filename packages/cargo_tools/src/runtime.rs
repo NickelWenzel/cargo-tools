19ce48e2cf2890d9436a69e07d150098f17189a7
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2025-12-20 12:16:45 +0100

    chore: thin out runtime

--- packages/cargo_tools/src/runtime.rs
+++ packages/cargo_tools/src/runtime.rs
@@ -1,8 +1,6 @@
 use futures::channel::mpsc::Receiver;
 use wasm_async_trait::wasm_async_trait;
 
-use crate::cargo_tools::state::{State, StateUpdate};
-
 #[derive(Debug, Clone)]
 pub struct Settings;
 
@@ -16,10 +14,4 @@ pub trait Runtime: 'static {
 
     fn current_dir_notitifier() -> Receiver<String>;
     fn file_changed_notifier(file: String) -> Receiver<()>;
-
-    async fn update_state_context(ctx: String) -> State;
-    async fn update_state(update: StateUpdate) -> State;
-
-    async fn update_settings_context(ctx: String) -> Settings;
-    async fn update_settings(update: SettingsUpdate) -> Settings;
 }
