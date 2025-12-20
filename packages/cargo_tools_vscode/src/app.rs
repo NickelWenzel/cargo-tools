999e54a8da9794c81322bc94821c96541223139a
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2025-12-20 17:01:11 +0100

    chore: cleanup and preparation

---
+++ packages/cargo_tools_vscode/src/app.rs
@@ -0,0 +1,2 @@
+pub mod cargo_make_ui;
+pub mod cargo_settings_ui;
--- packages/cargo_tools_vscode/src/lib.rs
+++ packages/cargo_tools_vscode/src/lib.rs
@@ -1,4 +1,5 @@
+pub mod app;
+pub mod context;
 pub mod contributes;
+pub mod runtime;
 pub mod vs_code_api;
-pub mod vs_code_cargo_tools;
-pub mod vs_code_state_manager;
