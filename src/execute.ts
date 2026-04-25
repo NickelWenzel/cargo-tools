574aff09a11c71b236024aec9c67f7e084d005ce
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2026-04-25 20:05:22 +0200

    chore: remove obsolete typescript code

--- src/execute.ts
+++ src/execute.ts
@@ -1,5 +1,6 @@
-import { exec, spawn } from 'child_process';
 import * as vscode from 'vscode';
+import { spawn } from 'child_process';
+import { VsCodeTask } from './wasm/cargo_tools_vscode';
 
 export async function echo_task(msg: string) {
     const task = new vscode.Task(
@@ -59,4 +60,41 @@ export async function showInformationMessage(message: string, items: string[]): 
 
 export async function showErrorMessage(message: string, items: string[]): Promise<string | undefined> {
     return await vscode.window.showErrorMessage(message, ...items);
+}
+
+export async function execute_task(cargo_tools_task: VsCodeTask): Promise<void> {
+    const cmd = cargo_tools_task.cmd();
+    const args = cargo_tools_task.args();
+
+    const definition: vscode.TaskDefinition = {
+        type: cargo_tools_task.task_type(),
+        args: args,
+    };
+
+    const execution = new vscode.ShellExecution(cmd, args);
+
+    const task = new vscode.Task(
+        definition,
+        vscode.TaskScope.Workspace,
+        `${cmd} ${args.join(" ")}`,
+        definition.type,
+        execution,
+        ['$rustc']
+    );
+
+    task.presentationOptions = {
+        echo: true,
+        reveal: vscode.TaskRevealKind.Always,
+        focus: false,
+        panel: vscode.TaskPanelKind.Shared,
+        showReuseMessage: true,
+        clear: false
+    };
+
+    try {
+        await vscode.tasks.executeTask(task);
+    } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        vscode.window.showErrorMessage(`Failed to run cargo make task: ${message}`);
+    }
 }
