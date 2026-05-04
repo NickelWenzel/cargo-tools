4f33da7148ca0b59974555e51bbb81b92827f5b6
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2026-05-04 19:22:36 +0200

    chore: move extension code to cargo_tools_vscode crate

--- src/types/webassembly.d.ts
+++
@@ -1,8 +0,0 @@
-// WebAssembly type declarations for wasm-bindgen generated code
-declare namespace WebAssembly {
-    interface Memory {
-        readonly buffer: ArrayBuffer;
-    }
-
-    interface Table { }
-}
