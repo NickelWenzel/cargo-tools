20998723e2615328ad5968e80157e2c33e41dc46
Nicolas Wenzel <nicolas.wenzel@gmx.de>
2026-05-22 18:13:56 +0200

    chore: remove obsolete cargo commands file

--- packages/cargo_tools_vscode/src/commands/mod.rs
+++ packages/cargo_tools_vscode/src/commands/mod.rs
@@ -1,7 +1,6 @@
 /// Commands are defined in this extra module
 /// which supports also native targets so we can
 /// run tests that make sure that all commands in package.json are also implemented
-pub mod cargo;
 pub mod cargo_make;
 pub mod configuration;
 pub mod outline;
